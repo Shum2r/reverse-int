d3d144652972925c1944188d63f5a84b60381dc0
shum2r <shadel.de2r@disroot.org>
2023-09-17 04:00:57 +0300

    feat: the task is solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+
+    if (n < 0) {
+        n = -n;
+    }
+
+    let convertN = String(n);
+    
+    for (i = convertN.length - 1; i >= 0; i--) {
+    result += `${convertN[i]}`;
+  }
+    return +result;
 }
